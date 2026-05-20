0c30a12f14d7b629e534a8e8e82e4ae75bc1b2ff
Phuc Doan <89180888+emberfox205@users.noreply.github.com>
2026-05-20 22:34:48 +0200

    Initial commit

---
+++ src/main/java/frauas/teilr/TeilrApplication.java
@@ -0,0 +1,13 @@
+package frauas.teilr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeilrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeilrApplication.class, args);
+    }
+
+}
