87e8c5c1c034d6698c79bef800b9b5e01dd0792d
KhoaTranNguyen <10423060@student.vgu.edu.vn>
2026-07-01 13:46:53 +0200

    add: AppBroadcaster class

---
+++ src/main/java/frauas/teilr/service/AppBroadcaster.java
@@ -0,0 +1,33 @@
+package frauas.teilr.service;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppBroadcaster {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public AppBroadcaster(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    // 1. Group Detail Screen Updates
+    public void broadcastGroupDetailUpdate(Long groupId) {
+        messagingTemplate.convertAndSend("/topic/group/" + groupId + "/detail", "update");
+    }
+
+    // 2. User Home Screen Updates (Group Lists)
+    public void broadcastUserHomeUpdate(Long userId) {
+        messagingTemplate.convertAndSend("/topic/user/" + userId + "/home", "update");
+    }
+
+    // 3. User Friends Screen Updates
+    public void broadcastFriendUpdate(Long userId) {
+        messagingTemplate.convertAndSend("/topic/user/" + userId + "/friends", "update");
+    }
+
+    // 4. Registration Verification Updates
+    public void broadcastEmailVerified(Long userId) {
+        messagingTemplate.convertAndSend("/topic/user/" + userId + "/verified", "update");
+    }
+}
